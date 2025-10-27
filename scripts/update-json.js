e0880a33150fa3cf40e181727c3f7942a8e7f3d2
Your Name <you@example.com>
2025-10-27 19:41:10 +0100

    subida 7

--- scripts/update-json.js
+++ scripts/update-json.js
@@ -4,7 +4,7 @@ const file = "./data.json";
 const data = JSON.parse(fs.readFileSync(file, "utf8"));
 
 // Modificar valores visibles
-data.counter = (data.counter || 0) + 1;
+data.counter = typeof data.counter === "number" ? data.counter + 1 : 0;
 data.updated_at = new Date().toISOString();
 data.random_number = Math.floor(Math.random() * 10000);
 
